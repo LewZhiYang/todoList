adaa46b4f5879a2ad2ace95cd8de7d65f237828c
LewZhiYang <circle1329@gmail.com>
2022-02-07 12:17:26 +0800

    used .env file

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+
+require('dotenv').config({path: '.env'})
+// this alternative also works 
+// const env = require('dotenv')
+// env.config({path: '.env'})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(8000)
+app.listen(process.env.PORT)
